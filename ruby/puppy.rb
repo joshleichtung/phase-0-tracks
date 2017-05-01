a6dad5f1e5013d05adca0e33ed1cce26651e359d
Josh Leichtung <joshleichtung@gmail.com>
2017-05-01 16:48:18 -0700

    Add Puppy class decription

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# SPECIES ------------------------
+# Canis lupus familiaris
+#
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Nose: wet
+# legs: 4
+#
+# BEHAVIOR -----------------------
+# Jump
+# Tail Wag
+# Bark
+# Lick
+
