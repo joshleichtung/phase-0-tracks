5fb43c399779050d1c1396ddc7cc85e7d8663485
Josh Leichtung <joshleichtung@gmail.com>
2017-05-02 13:25:22 -0700

    Add qseudocode for grocery list GPS 2.2

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,29 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Split the string by spaces to turn it into an array
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [array of hashes where the hash has a key for the item name and for
+# the quantity]
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps: push a hash of the item and the qantity onto the original list
+# output: modified array
+
+# Method to remove an item from the list
+# input: list and string of the item name
+# steps: Look up item in the list, and delete
+# output: new list/array with the item deleted
+
+# Method to update the quantity of an item
+# input: list, string of the item, and quantity with default value 1
+# steps: push hash of string and quanatity
+# output: new array/list
+
+# Method to print a list and make it look pretty
+# input: array/list
+# steps: Iterate through list and print Item name and Quantity for each item on
+# a a line. Print with string interpolation
+# output: list
