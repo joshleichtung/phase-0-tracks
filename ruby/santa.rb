57b03a969031c9cc402cf5a1278829d549ebdb2a
Josh Leichtung <joshleichtung@gmail.com>
2017-05-02 12:17:44 -0700

    Complete Release 2 - Assignment 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,12 @@
 class Santa
-  def initialize
+  def initialize(gender = "N/A", ethnicity = "N/A")
     puts "Initializing Santa instance..."
+    @gender = gender
+    @ethnicity = ethnicity
   end
+
+  @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  @age = 0
 
   def speak
     puts "HO,ho, ho! Haaaappy holidays!"
@@ -17,3 +22,13 @@ santa = Santa.new
 santa.speak
 santa.eat_milk_and_cookies("Oreo")
 
+facebook_genders = ["Agender", "Androgyne", "Androgynous", "Bigender", "Cis", "Cisgender", "Cis Female", "Cis Male", "Cis Man", "Cis Woman", "Cisgender Female", "Cisgender Male", "Cisgender Man", "Cisgender Woman", "Female to Male", "FTM", "Gender Fluid", "Gender Nonconforming", "Gender Questioning", "Gender Variant", "Genderqueer", "Intersex", "Male to Female", "MTF", "Neither", "Neutrois", "Non-binary", "Other", "Pangender", "Trans", "Trans*", "Trans Female", "Trans* Female", "Trans Male", "Trans* Male", "Trans Man", "Trans* Man", "Trans Person", "Trans* Person", "Trans Woman", "Trans* Woman", "Transfeminine", "Transgender", "Transgender Female", "Transgender Male", "Transgender Man", "Transgender Person", "Transgender Woman", "Transmasculine", "Transsexual", "Transsexual Female", "Transsexual Male", "Transsexual Man", "Transsexual Person", "Transsexual Woman", "Two-Spirit"]
+
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+
+santas = []
+20.times do
+  santas.push Santa.new(facebook_genders[rand(facebook_genders.length)], example_ethnicities[rand(example_ethnicities.length)])
+end
+
+santas.each {|santa| p santa}
