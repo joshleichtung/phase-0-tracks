372070fceaab1bf63142286b3319e2379b4a66c8
Josh Leichtung <joshleichtung@gmail.com>
2017-05-01 18:45:47 -0700

    Complete Release 0 Assignment 6.2

--- ruby/puppy.rb
+++ ruby/puppy.rb
@@ -17,9 +17,34 @@
 # Lick
 
 class Puppy
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
 
+  def speak(n)
+    n.times { puts "Woof!" }
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(x)
+    x * 7
+  end
+
+  def shake_hands
+    puts "*holds out paw*"
+  end
 end
 
-duchess = Puppy.new
+###############Driver###############
+
 fido = Puppy.new
-spot = Puppy.new
+ball = "ball"
+p fido.fetch(ball)
+fido.speak(5)
+fido.roll_over
+p "3 human years is #{fido.dog_years(3)} dog years"
+fido.shake_hands
