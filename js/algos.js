24a77fb67e1b242e09c45593aa709c04c50e75b2
Josh Leichtung <joshleichtung@gmail.com>
2017-05-08 13:18:31 -0700

    Add longest_phrase js function for Assignment 7.3 Release 0

---
+++ js/algos.js
@@ -0,0 +1,23 @@
+/* Longest phrase function
+ * Parameters: array
+ * Return: string
+ * variable result set equal to first phrase
+ * FOR loop through array starting with index 1
+ *  IF length of array at index is greater than current result length
+ *    Set result equal to string at array index
+*/
+
+function longest_phrase(ary){
+  var result = ary[0];
+  for(var i = 1; i < ary.length; i++){
+    if (ary[i].length > result.length){
+      result = ary[i];
+    }
+  }
+  return result;
+}
+
+var words = ['one', 'two', 'three'];
+console.log("Should be three: " + longest_phrase(words));
+var phrases = ["short", "a medium phrase", "this is a pretty long phrase", "this is a really super long phrase, and it should be the longest"];
+console.log(longest_phrase(phrases));
