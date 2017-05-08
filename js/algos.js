9c939d37317ec554bce5d7f25ad12d239925648c
Josh Leichtung <joshleichtung@gmail.com>
2017-05-08 16:09:26 -0700

    Add matching_key method for Assignment 7.3 Release 1

--- js/algos.js
+++ js/algos.js
@@ -21,3 +21,26 @@ var words = ['one', 'two', 'three'];
 console.log("Should be three: " + longest_phrase(words));
 var phrases = ["short", "a medium phrase", "this is a pretty long phrase", "this is a really super long phrase, and it should be the longest"];
 console.log(longest_phrase(phrases));
+
+
+/* shared pair function
+ * Parameters: 2 objects
+ * got object keys of first object.
+ * Iterate over object keys to see if other object has matching value
+ *
+*/
+
+function matching_key(obj1, obj2){
+  for(key in obj1){
+    console.log("KEY: " + key);
+    if (obj1.hasOwnProperty(key) && obj1[key] === obj2[key]){
+      return true;
+    }
+  }
+  return false
+}
+
+obj1 = {one: 1, two: "too", three: "a crowd"};
+obj2 = {one: 1, too: 2, three: 4};
+obj3 = {dogs: "cool", cats: "lame", hamsters: "meh"};
+
