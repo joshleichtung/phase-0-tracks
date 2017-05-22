881014e1b3c249d2e18111e9e8cb6447cc678aa6
Josh Leichtung <joshleichtung@gmail.com>
2017-05-22 10:10:52 -0700

    Add secret message when clicking #header

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -2,6 +2,8 @@ var header = document.getElementById("header");
 
 function changeTextColor(event){
   event.target.style.color = "blue";
+  var text = document.createTextNode("You have unlocked a secret message");
+  event.target.parentNode.appendChild(text);
 }
 
 header.addEventListener("click", changeTextColor);
